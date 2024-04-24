cdf1861c7435dd95ebb82baa8839ee1c2db0fc8a
shrutikhadake <148204864+shrutikhadake@users.noreply.github.com>
2024-04-24 09:08:31 +0530

    Update Even_odd.java

--- Even_odd.java
+++ Even_odd.java
@@ -1,15 +1,18 @@
-class FibonacciExample1{  
-public static void main(String args[])  
-{    
- int n1=0,n2=1,n3,i,count=10;    
- System.out.print(n1+" "+n2);//printing 0 and 1    
-    
- for(i=2;i<count;++i)//loop starts from 2 because 0 and 1 are already printed    
- {    
-  n3=n1+n2;    
-  System.out.print(" "+n3);    
-  n1=n2;    
-  n2=n3;    
- }    
-  
-}}  
+import java.util.*;
+public class even_odd {
+    public static void main(String arg[])
+    {
+        int num;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number:");
+        num= sc.nextInt();
+        if(num%2==0)
+        {
+            System.out.println("The given number is even");
+        }
+        else {
+            System.out.println("The given number is odd");
+        }
+    }
+
+}
